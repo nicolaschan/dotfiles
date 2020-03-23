6970d85200125a456ad68bcf90bd69379855ca81
Nicolas Chan <nicolas@nicolaschan.com>
2020-03-22 21:07:44 -0700

    Add minecraft server reference scripts

---
+++ minecraft/do-backup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+MC=$HOME/mc
+MC_BACKUP=$MC/minecraft-backup
+SERVERS=$MC/active
+
+$MC_BACKUP/backup.sh -a zstd -e zst -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o $MC/backups
+export RESTIC_PASSWORD_FILE=$MC/restic-password.txt
+$MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o sftp:intranet:/mnt/extstorage/backups/privatesurvival-restic
+$MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o sftp:francis:harddrive2/backups/minecraft-restic
+$MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o sftp:randall:minecraft-restic
+$MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o rclone:berkeley-drive-enc:/minecraft-restic
+$MC_BACKUP/backup-restic.sh -g -c -s atm3 -i $SERVERS/atm3/world -o sftp:intranet:/mnt/extstorage/backups/privatesurvival-restic
+$MC_BACKUP/backup-restic.sh -g -c -s atm3 -i $SERVERS/atm3/world -o rclone:berkeley-drive-enc:/minecraft-restic
