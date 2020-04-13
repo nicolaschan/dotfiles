d5f90a937fe5a556b996fb3e74c829d3b573d3ea
Nicolas Chan <nicolas@nicolaschan.com>
2020-04-12 19:29:23 -0700

    Add smartcard

--- minecraft/do-backup.sh
+++ minecraft/do-backup.sh
@@ -5,6 +5,7 @@ SERVERS=$MC/active
 
 $MC_BACKUP/backup.sh -a zstd -e zst -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o $MC/backups
 export RESTIC_PASSWORD_FILE=$MC/restic-password.txt
+$MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o $MC/minecraft-restic
 $MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o sftp:intranet:/mnt/extstorage/backups/privatesurvival-restic
 $MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o sftp:francis:harddrive2/backups/minecraft-restic
 $MC_BACKUP/backup-restic.sh -g -c -s privatesurvival -i $SERVERS/privatesurvival/world -o sftp:randall:minecraft-restic
