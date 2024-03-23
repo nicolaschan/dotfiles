cfae0d7cc0d41bbcdb968d30cda6b316b0dbd75d
Nicolas Chan <nicolas@nicolaschan.com>
2024-03-23 13:30:40 -0700

    Add fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/nicolas/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+zoxide init --cmd cd fish | source
+#alias cd=z
+alias ls=lsd
+alias cat=ccat
+#alias ping="prettyping --nolegend"
+fzf_key_bindings
