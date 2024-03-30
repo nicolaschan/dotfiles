b68373ecd0275a3b1652dc5e83b75e4c507acb6d
Nicolas Chan <nicolas@nicolaschan.com>
2024-03-29 22:09:36 -0700

    Disable conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,12 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/nicolas/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+#eval /home/nicolas/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 zoxide init --cmd cd fish | source
 #alias cd=z
 alias ls=lsd
-alias cat=ccat
+#alias cat=ccat
 #alias ping="prettyping --nolegend"
 fzf_key_bindings
