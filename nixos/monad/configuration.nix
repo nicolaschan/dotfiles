531fd703203bf016c7bd42ebb1f17640927fbdfb
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-06 00:41:40 -0700

    NixOS configuration: monad generation system-28-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -137,7 +137,6 @@
     cudatoolkit
     git
     linuxPackages.nvidia_x11
-(nvtop.override { nvidia = true; })
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
