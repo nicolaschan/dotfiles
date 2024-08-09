9dbe96c1fa63921fb27b50085360b0b1de1700fb
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-08 23:06:06 -0700

    NixOS configuration: monad generation system-40-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -243,4 +243,6 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  system.autoUpgrade.enable = true;
 }
