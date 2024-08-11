720ef46e9e428ac83987f7fe1384d4110af8212a
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-11 11:44:00 -0700

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -17,7 +17,7 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  fonts.fontconfig.enableProfileFonts = true;
+  fonts.fontconfig.enable = true;
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
