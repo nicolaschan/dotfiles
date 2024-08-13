465633763a2271af1ec3efd17adea30e63281f7c
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-12 20:26:41 -0700

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -90,8 +90,8 @@
 
     # Fonts
     cascadia-code
-    nerdfonts
     noto-fonts
+    noto-fonts-emoji
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
