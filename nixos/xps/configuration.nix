347954d28c41b26bfaf69522b98aeaa64d4923f5
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-27 08:52:31 -0700

    NixOS configuration: xps generation system-108-link

--- nixos/xps/configuration.nix
+++ nixos/xps/configuration.nix
@@ -217,4 +217,10 @@ in {
   system.stateVersion = "23.11"; # Did you read the comment?
 
   system.autoUpgrade.enable = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
