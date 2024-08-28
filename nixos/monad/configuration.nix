75e19dd9896ff8418264de1cdc5712053aaaa219
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-27 21:38:16 -0700

    NixOS configuration: monad generation system-55-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -143,6 +143,7 @@
     git
     libnvidia-container
     linuxPackages.nvidia_x11
+    nvidia-container-toolkit
     nvtopPackages.nvidia
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
