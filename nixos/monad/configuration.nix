fb52ea575ba1ea80859a98919911354bec221515
Nicolas Chan <nicolas@nicolaschan.com>
2024-09-11 22:23:50 -0700

    NixOS configuration: monad generation system-110-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -129,6 +129,7 @@
     uid = 1002;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPlM2ilPhTvNX0DoYiU+o3+HRsU3dtHGcZ0igWf3cqR4 nicolas@xn--h98h"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtEyrE++I+HzN3nrCVqEWdyVxPikPJ6XzoFxnLxk4ML nicolas@monad"
     ];
   };
 
