7ba793ad3ed6e02aed5fb460ee42115787655ca6
Nicolas Chan <nicolas@nicolaschan.com>
2024-09-16 23:13:09 -0700

    NixOS configuration: monad generation system-123-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -221,6 +221,12 @@
     settings = {
       PermitRootLogin = "prohibit-password";
       PasswordAuthentication = false;
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-512" # For compatibility with passforios
+      ];
     };
   };
 
