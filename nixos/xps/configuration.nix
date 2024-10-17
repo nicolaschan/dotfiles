236587ee4453959f094925e77c659b1d6e09262f
Nicolas Chan <nicolas@nicolaschan.com>
2024-10-16 22:27:56 -0700

    NixOS configuration: xps generation system-155-link

--- nixos/xps/configuration.nix
+++ nixos/xps/configuration.nix
@@ -74,7 +74,7 @@ in {
       config = ''
         (defsrc caps)
         (deflayermap (default-layer)
-          caps (tap-hold 50 50 esc lctl))
+          caps (tap-hold 100 100 esc lctl))
       '';
     };
   };
