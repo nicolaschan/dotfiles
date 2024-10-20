112a8233e83841b5f1de2b133b75caa4bd31e3cb
Nicolas Chan <nicolas@nicolaschan.com>
2024-10-19 20:53:19 -0700

    NixOS configuration: monad generation system-146-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -16,6 +16,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.configurationLimit = 10;
+
   networking.hostName = "monad"; # Define your hostname.
   networking.hostId = "4dc56713";
   # Pick only one of the below networking options.
