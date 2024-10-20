a85325fbad48698208f332de493dd4398f8fd67c
Nicolas Chan <nicolas@nicolaschan.com>
2024-10-19 20:07:22 -0700

    NixOS configuration: monad generation system-145-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -54,7 +54,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
