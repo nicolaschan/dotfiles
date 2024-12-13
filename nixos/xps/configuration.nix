4be9e1450f491c22970d6db57b9b54c25184177c
Nicolas Chan <nicolas@nicolaschan.com>
2024-12-12 19:03:16 -0800

    NixOS configuration: xps generation system-195-link

--- nixos/xps/configuration.nix
+++ nixos/xps/configuration.nix
@@ -47,12 +47,6 @@ in {
     LC_TIME = "en_US.UTF-8";
   };
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = with pkgs; [fcitx5-rime fcitx5-chinese-addons fcitx5-pinyin-zhwiki];
-  };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
