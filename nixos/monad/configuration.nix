69e24c4785f6716c99627521a7db585a4280ff4d
Nicolas Chan <nicolas@nicolaschan.com>
2025-04-12 10:03:52 -0700

    NixOS configuration: monad generation system-194-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -53,7 +53,9 @@
   # };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
