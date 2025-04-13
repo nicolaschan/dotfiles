39819ccd94653caf7a9a9545d8b37cbf05f4e50f
Nicolas Chan <nicolas@nicolaschan.com>
2025-04-13 13:04:33 -0700

    NixOS configuration: monad generation system-206-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -216,10 +216,14 @@
     "acpi_osi=\"!Windows 2020\"" # Use Linux-specific ACPI implementation
     "usb-storage.quirks=2188:0035:u,2188:0032:u,2188:0031:u"
     "pciehp.pciehp_force=1"
+    "usb-storage.delay_use=0"
+    "usbcore.autosuspend=-1"
+    "usbhid.mousepoll=0"
   ];
   boot.extraModprobeConfig = ''
     options usbcore autosuspend=-1
     options xhci_hcd quirks=0x80
+    options typec altmodes=1
   '';
   boot.kernel.sysctl = {
     "vm.dirty_ratio" = 6;
