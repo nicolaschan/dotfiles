19bfb586ca99b49e1218a03cd1b948ed058bffe9
Nicolas Chan <nicolas@nicolaschan.com>
2025-05-24 11:56:23 -0700

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -13,7 +13,8 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
     fonts.monospace = {
       package = pkgs.cascadia-code;
       name = "Cascadia Code PL";
