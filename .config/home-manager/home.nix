0dfeaa6664d318536354e414b5bd05aaa9f1ff56
Nicolas Chan <nicolas@nicolaschan.com>
2025-05-24 11:07:54 -0700

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -12,7 +12,7 @@
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
     fonts.monospace = {
