58de14943fd52d7c2058a37df8767c2e27a0d34b
Nicolas Chan <nicolas@nicolaschan.com>
2025-05-24 09:38:27 -0700

    Add xps system flake

---
+++ nixos/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${"x86_64-linux"};
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      xps = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs;
+          inherit pkgs-unstable;
+        };
+        # > Our main nixos configuration file <
+        modules = [
+          ./xps/configuration.nix
+        ];
+      };
+    };
+  };
+}
