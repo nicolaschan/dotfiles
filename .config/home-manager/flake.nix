bbc1ed14595bff808a5fa5fa6945d3b49865e8cc
Nicolas Chan <nicolas@nicolaschan.com>
2025-05-24 10:32:58 -0700

    Update home.nix

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -11,23 +11,32 @@
     insanity = {
       url = "github:nicolaschan/insanity";
     };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, insanity, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."nicolas" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = {
+    nixpkgs,
+    home-manager,
+    insanity,
+    stylix,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."nicolas" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [stylix.homeModules.stylix ./home.nix];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-        extraSpecialArgs = { inherit insanity; };
-      };
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = {inherit insanity;};
     };
+  };
 }
