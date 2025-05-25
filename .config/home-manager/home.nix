794b482acfe31f604b266d5804ed8b6ae207515a
Nicolas Chan <nicolas@nicolaschan.com>
2025-05-24 19:18:38 -0700

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -250,6 +250,7 @@
       telemetry = {
         metrics = false;
       };
+      assistant.enable = false;
       vim_mode = true;
       autosave = "on_focus_change";
       format_on_save = "on";
