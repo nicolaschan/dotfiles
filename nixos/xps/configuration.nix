b7d35a5154b20279b6b27f45ea71f4dc50034a79
Nicolas Chan <nicolas@nicolaschan.com>
2025-07-07 21:32:46 -0700

    NixOS configuration: xps generation system-35-link

--- nixos/xps/configuration.nix
+++ nixos/xps/configuration.nix
@@ -77,8 +77,8 @@ in {
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   programs.hyprland.enable = true;
 
   # Configure keymap in X11
