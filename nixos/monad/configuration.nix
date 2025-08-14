733039456ec43e6df909ca7a220f4dce96335290
Nicolas Chan <nicolas@nicolaschan.com>
2025-08-14 00:05:53 -0700

    NixOS configuration: monad generation system-276-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -21,10 +21,7 @@
   networking.hostId = "4dc56713";
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  networking.networkmanager = {
-    enable = true;
-  };
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   swapDevices = [
     {
