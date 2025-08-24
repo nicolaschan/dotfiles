3c520c890ed10fe6739d47ac80759806a0156e3c
Nicolas Chan <nicolas@nicolaschan.com>
2025-08-24 12:02:22 -0700

    NixOS configuration: xps generation system-49-link

--- nixos/xps/configuration.nix
+++ nixos/xps/configuration.nix
@@ -203,6 +203,7 @@ in {
         "--keep-monthly 6"
       ];
       timerConfig = {
+        Enable = false;
         OnCalendar = "20:00";
         RandomizedDelaySec = "1h";
         Persistent = true;
