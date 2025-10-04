881e8315a3fa658bcfab4f9ac9ee481a0573c052
Nicolas Chan <nicolas@nicolaschan.com>
2025-10-04 12:32:44 -0700

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -272,6 +272,7 @@
       features.copilot = true;
       # assistant.enabled = false;
       vim_mode = true;
+      vim.use_system_clipboard = false;
       autosave = "on_focus_change";
       format_on_save = "on";
       formatter = "language_server";
