5811512d3637f3f26baf6ac0396596a7788a2b28
Nicolas Chan <nicolas@nicolaschan.com>
2025-11-08 13:11:32 -0800

    NixOS configuration: monad generation system-306-link

--- nixos/monad/configuration.nix
+++ nixos/monad/configuration.nix
@@ -109,7 +109,7 @@
   services.k3s = {
     enable = true;
     role = "server";
-    extraFlags = "--disable=traefik --disable=metrics-server"; # --flannel-backend=vxlan";
+    extraFlags = "--disable=traefik"; # --disable=metrics-server"; # --flannel-backend=vxlan";
   };
 
   systemd.services.k3s = {
