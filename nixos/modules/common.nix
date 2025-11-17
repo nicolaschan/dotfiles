bb2054f325e0dfb3ca26c5aace4eca30c31d002c
Nicolas Chan <nicolas@nicolaschan.com>
2025-11-16 23:36:36 -0800

    Remove unnecessary sudo config

--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -145,17 +145,6 @@
       "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBDgWXcT8lPSDFOBCSGYwaUzal+1B0rPPuR5s9f4rpnY53KnIc8KnvonV4/0OrSLiAPndTyq8vMN5mv3x6zNbnpgAAAALdGVybWl1cy5jb20= nicolas@terminus-iphone"
     ];
   };
-  security.sudo.extraRules = [
-    {
-      users = [ "nicolas" ];
-      commands = [
-        {
-          command = "ALL";
-          # options = [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
