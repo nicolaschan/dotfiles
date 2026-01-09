f4ff29bdbebdb69eb15de342294122ad808178ad
Nicolas Chan <nicolas@nicolaschan.com>
2026-01-08 20:33:38 -0800

    Update nvidia settings

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -7,15 +7,10 @@
 {
   hardware.graphics = {
     enable = true;
-    enable32Bit = true; # required for docker.enableNvdia
+    enable32Bit = true;
   };
 
   hardware.nvidia-container-toolkit.enable = true;
-  hardware.nvidia-container-toolkit.mount-nvidia-executables = true;
-
-  environment.systemPackages = with pkgs; [
-    nvidia-container-toolkit
-  ];
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
