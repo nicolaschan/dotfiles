6913d0f145dcf323f6b59fd8b3f2408941bdcb5d
Nicolas Chan <nicolas@nicolaschan.com>
2026-03-11 08:43:37 -0700

    Revert "Update system flake"
    This reverts commit 52ca6d1b76e8d77c735be3963b11d5c5216452cb.

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,9 +1,13 @@
 {
   config,
+  pkgs,
   ...
 }:
 
 {
+  # Use kernel 6.18 until NVIDIA drivers support 6.19
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
